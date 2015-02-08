c244cdd34cd3786ad97adc7d9174b36957548f96
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-08 11:56:48 -0500

    round 2

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,13 @@
+class SearchesController < ApplicationController
+  ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
+  # ETSY_API_TOKEN = kn84tew7pjzr6upsxq0m864t
+  # SECRET = va4dv3nqsl
+  # GET /etsy_search => etsy#search
+  # GET /etsy_search
+  def search
+    search_results =  Etsy::Request.get('/listings/active',
+                                        :includes => ['Images', 'Shop'],
+                                        :keywords => 'whiskey')
+    @to_ruby = JSON.parse(search_results.body)
+  end
+end
