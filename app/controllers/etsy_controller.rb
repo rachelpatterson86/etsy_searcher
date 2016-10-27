60407dbdcef38ffddea14e5754ba2cee11b66db2
Rachel Patterson <rachel.patterson.1986@gmail.com>
2016-10-27 13:21:48 -0400

    Add sidebar panel; some refactoring; add filter by category for search.

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -8,10 +8,17 @@ class EtsyController < ApplicationController
     search_results =  Etsy::Request.get(
       '/listings/active',
       :includes => ['Images', 'Shop'],
-      :keywords => params[:q]
+      :keywords => params[:q],
+      :category => params[:c]
     )
 
     @listings = JSON.parse(search_results.body)['results']
+    @categories = EtsyController::all_categories
+    # binding.pry
     render :search
+  end
+
+  def self.all_categories
+    categories = Etsy::Category.all_top
   end
 end
